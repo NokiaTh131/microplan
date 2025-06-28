3a2217b4964ec484a6da492dfcd3fa0ee67c423b
= <=>
2025-06-28 15:52:04 +0700

    fix: Update base path configuration for production environment in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: process.env.NODE_ENV === "production" ? "/microplan/" : "/",
   server: {
     port: 80,
     host: true,
